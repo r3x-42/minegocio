47b168476ff41842d51cb77ec90db1434ff4f814
Andru <andruvargas023@gmail.com>
2023-02-03 18:28:59 -0600

    creacion de componente Main y sus estilos Desktop

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { LinksFunction, MetaFunction } from "@remix-run/node";
 import {
   Links,
   LiveReload,
@@ -7,6 +7,17 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+
+import styles from './styles/style.css';
+
+export const links: LinksFunction = () => {
+  return [
+    {
+      rel:"stylesheet",
+      href: styles,
+    },
+  ];
+};
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,8 +1,16 @@
+import Main from "~/components/Main";
+
 export default function Index() {
   return (
-    <div>
-      <h2>LimonX</h2>
-      <p>sitio web listo para la sesión 2</p>
+    <div style={{
+      background:`repeating-linear-gradient(0deg, rgba(137,134,134, 0.45) 0px, rgba(137,134,134, 0.45) 1px,transparent 1px, transparent 36px),repeating-linear-gradient(90deg, rgba(137,134,134, 0.45) 0px, rgba(137,134,134, 0.45) 1px,transparent 1px, transparent 36px),linear-gradient(90deg, hsl(334,2%,100%),hsl(334,2%,100%));`
+    }}>
+    <main className="max-w-[720px] mx-auto py-20">
+      <Main />
+    </main>
+
     </div>
+
+    
   );
 }
